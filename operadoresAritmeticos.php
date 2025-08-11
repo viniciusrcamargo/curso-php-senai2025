ac01bd72d5c5f7dc447d4e4faa82a4bc4d7e200a
Vinicius <viniciusr.camargo@gmail.com>
2025-08-11 09:52:07 -0300

    Exemplos do curso

---
+++ operadoresAritmeticos.php
@@ -0,0 +1,11 @@
+<?php
+$num1 = 10;
+$num2 = 35;
+//adição +
+//subtração -
+//multiplicação *
+//divisão /
+echo ($num1 + $num2)/100 .PHP_EOL;
+echo ($num1 - $num2) .PHP_EOL;
+echo ($num1 * $num2) .PHP_EOL;
+echo ($num1 / $num2) .PHP_EOL;
