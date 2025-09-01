3019270859238ddd95cb90f7159957117200131a
Vinicius <viniciusr.camargo@gmail.com>
2025-09-01 13:55:55 -0300

    Curso PHP

--- pdoCon.php
+++ pdoCon.php
@@ -1,24 +1,33 @@
 <?php
-// Configurações do banco de dados
-$servidor = "localhost";
-$banco = "seu_banco";
-$usuario = "seu_usuario";
-$senha = "sua_senha";
-$charset = "utf8mb4";
 
-// Data Source Name (DSN)
-$dsn = "mysql:host=$servidor;dbname=$banco;charset=$charset";
+class PdoCon
+{
+    private $usuario = 'root';
+    private $senha = 'Senai@901';//
+    private $banco = 'contatos';
+    private $servidor = 'localhost';//máquina local
+    private $charset = "utf8mb4";
+    private static $pdo;//que não muda
 
-$opcoes = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
-];
 
-try {
-    $pdo = new PDO($dsn, $usuario, $senha, $opcoes);
-    echo "Conectado com sucesso via PDO!";
-} catch (\PDOException $e) {
-    throw new \PDOException($e->getMessage(), (int)$e->getCode());
+    //método de conexão
+    public function conectar()
+    {
+        if(is_null(self::$pdo)){//verifica se não tem estância criada
+            try{//mysql:host=$servidor;dbname=$banco;
+                $dsn = "mysql:host=" . (new self())->servidor . ";dbname=" .
+                    (new self())->banco .";charset=" . (new self())->charset;
+                self::$pdo = new Pdo($dsn, (new self())->usuario, (new self())->senha);
+                //echo "Conectado com sucesso!";
+            }catch (PDOException $e){
+                die("Erro de conexão" . $e->getMessage(). "\n");
+            }
+        }
+        return self::$pdo;
+    }
+
 }
-?>
+
+//só para testes
+$conexao = new PdoCon();
+$conexao->conectar();
